aaa9ba045e9a3b88f2c97fd36c430e4215fc3bb8
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-24 22:26:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ client.on("messageCreate", async (message) =>{ //whenever a message is created t
             const heHimRole = message.guild.roles.cache.some(r => r.name === 'he');
             const sheHerRole = message.guild.roles.cache.some(r => r.name === 'she');
             
-            if (message.member.roles.has(heHimRole)){
+            if (message.member.roles.some(heHimRole)){
                 const royCoinEmbedReward = new MessageEmbed()
                 .setColor('#ffff00')
                 .addFields(
@@ -87,7 +87,7 @@ client.on("messageCreate", async (message) =>{ //whenever a message is created t
                 message.author.send({ embeds: [royCoinEmbedReward] });
 
             }
-            else if (message.member.roles.has(sheHerRole)){
+            else if (message.member.roles.some(sheHerRole)){
                 const royCoinEmbedReward = new MessageEmbed()
                 .setColor('#ffff00')
                 .addFields(
