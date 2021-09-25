3f39eb328ec4e9e74fbd16cd9ce6ff6a8ba59136
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-25 10:58:47 +0100

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -11,9 +11,7 @@ module.exports = {
         const id = profileData.userID;
         const coins = profileData.coins;
 
-        coins
-        .cache
-        .get({serverID: message.guild.id})
+        coins.cache.get({serverID: message.guild.id})
         .sort([['coins', 'decending']])
         .exec((err, res) => {
             if(err) console.log(err);
