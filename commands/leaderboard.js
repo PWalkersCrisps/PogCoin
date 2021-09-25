6d8442ab19115cbb395f3e6d021a9a2df746683a
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-25 08:30:03 +0100

    Update leaderboard.js
    added LB functionality and warnings

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,30 +1,40 @@
+//THIS IS NOT MY CODE ITS FROM A TUTORIAL, IF YOU CAN FIND BETTER CODE, PLEASE DO MODIFY THIS, I WILL APPRECIATE IT A LOT
+//TUTORIAL: https://www.youtube.com/watch?v=NFOoUhiUrbk
+
+//I DO HAVE TO MENTION IF THIS IS BEING EXPANDED APON, THIS IS NOT VERY EFFICIENT, THE MORE USERS IT HAS TO READ THEN THE LONGER IT WILL TAKE
+
+
+const { Collection } = require("discord.js");
+
 module.exports = {
     name: "leaderboard",
     description: "Displays the top 10 users",
     async execute(client, message, args, Discord, profileData, MessageEmbed){
 
 
+        const leaderBoardCollection = new Collection()
 
+        await Promise.all(
+            message.guild.members.cache.map(async(member) => {
+                const id = profileData.userID;
+                const coins = profileData.coins;
+                return coins !== 0 ? Collection.set(id, {
+                    id,
+                    bal,
+                })
+                :null
+            })
+        )
 
-
-
-
+        const data = Collection.sort((a, b) => b.coins - a.coins).first(10)
 
         const royCoinLeaderBoard = new MessageEmbed() //Starts the proccess for creating an embed
         .setColor('#6603fc')
         .setTitle('Roy Coin Leaderboards')
-        .setDescription('Some description here') //Temp description, ig just write something creative here
-        .addFields(
-            { name: `1`, value: `1`},
-            { name: `2`, value: `2`},
-            { name: `3`, value: `3`},
-            { name: `4`, value: `4`},
-            { name: `5`, value: `5`},
-            { name: `6`, value: `6`},
-            { name: `7`, value: `7`},
-            { name: `8`, value: `8`},
-            { name: `9`, value: `9`},
-            { name: `10`, value: `10`},
+        .setDescription(
+            data.map((v, i) => {
+                return `${i+1}) ${client.user.cache.get(v.id).tag} => ${v.coins} Roy Coins`
+            })
         )
         .setTimestamp()
         .setFooter('Reddit Gold Replacement?');    
