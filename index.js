ef8c86e508bbc6fb979734a9514f972d5fa2a8e3
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-26 08:35:41 +0100

    Made the if/else chain more efficient

--- index.js
+++ index.js
@@ -78,36 +78,30 @@ client.on("messageCreate", async (message) =>{ //whenever a message is created t
                 }
             });
 
+            const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
+            .setColor('#ffff00')
+            .setTimestamp()
+            .setFooter('Reddit Gold Replacement?');    
+
+
             if (message.member.roles.cache.some(role => role.name === 'he')){ //checks if the auther has the he/him role
-                const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
-                .setColor('#ffff00')
-                .addFields(
+                royCoinEmbedReward.addFields(
                     { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good boy`},
                 )
-                .setTimestamp()
-                .setFooter('Reddit Gold Replacement?');    
                 message.author.send({ embeds: [royCoinEmbedReward] }); //sends the embed that was just created 
 
             }
             else if (message.member.roles.cache.some(role => role.name === 'her')){ //checks if the auther has the she/her role
-                const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
-                .setColor('#ffff00')
-                .addFields(
+                royCoinEmbedReward.addFields(
                     { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good girl`},
                 )
-                .setTimestamp()
-                .setFooter('Reddit Gold Replacement?');   
                 message.author.send({ embeds: [royCoinEmbedReward] }); //sends the embed that was just created
  
             }
             else{ //If the user has the they/them or dont have a gender role, it will always default to this
-                const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
-                .setColor('#ffff00')
-                .addFields(
+                royCoinEmbedReward.addFields(
                     { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good child`},
                 )
-                .setTimestamp()
-                .setFooter('Reddit Gold Replacement?');    
                 message.author.send({ embeds: [royCoinEmbedReward] }); //sends the embed that was just created
                 
             }
