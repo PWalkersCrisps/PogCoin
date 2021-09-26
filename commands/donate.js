a9f2e4e730ccb928896993283f6792a9b31e7b0c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-26 13:39:32 +0100

    Create donate.js

---
+++ commands/donate.js
@@ -0,0 +1,38 @@
+const profileModel = require("../models/profileScema");
+
+module.exports = {
+    name: "donate",
+    description: "pings the server to see the delay between the client and the server",
+    async execute(client, message, args, Discord, profileData, MessageEmbed){
+
+        let mentionedUser = message.mentions.users.first();
+        if (!mentionedUser) return message.channel.send('You need to mention a user.');
+
+        const response = await profileModel.findOneAndUpdate({
+            userID: message.author.id,
+        }, {
+            $inc: {
+                coins: -1,
+            }
+        });
+        
+        profileDataMentioned = await profileModel.findOne({userID: message.mentionedUser.id});
+        if(!profileDataMentioned) //Checks if the user has any data in the DB
+        {
+            let newUser = await profileModel.create({
+                userID: message.mentionedUser.id,
+                coins: 0,
+            });
+            //const savedUser = await newUser.save();
+        }
+
+        const response = await profileModel.findOneAndUpdate({
+            userID: message.mentionedUser.id,
+        }, {
+            $inc: {
+                coins: 1,
+            }
+        });
+
+    }
+}
