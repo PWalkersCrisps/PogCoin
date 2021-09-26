2f49418ddba7a8984bf8ba6992f03868254a3041
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-26 15:26:11 +0100

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -6,10 +6,8 @@ module.exports = {
     description: "pings the server to see the delay between the client and the server",
     async execute(client, message, args, Discord){
 
-        let mentionedUser = message.mentions.users.first().id;
+        
         if (!mentionedUser) return message.channel.send('You need to mention a user.');
-
-        console.log(mentionedUser)
 
         const senderResponse = await profileModel.findOneAndUpdate({
             userID: message.author.id,
@@ -19,18 +17,18 @@ module.exports = {
             }
         });
         
-        profileDataMentioned = await profileModel.findOne({userID: message.mentionedUser});
+        profileDataMentioned = await profileModel.findOne({userID: message.mentions.users.first().id});
         if(!profileDataMentioned) //Checks if the user has any data in the DB
         {
             let newUser = await profileModel.create({
-                userID: message.mentionedUser,
+                userID: message.mentions.users.first().id,
                 coins: 0,
             });
             //const savedUser = await newUser.save();
         }
 
         const reciverResponse = await profileModel.findOneAndUpdate({
-            userID: message.mentionedUser,
+            userID: message.mentions.users.first().id,
         }, {
             $inc: {
                 coins: 1,
@@ -41,7 +39,7 @@ module.exports = {
         .setColor('#00ffff')
         .setTimestamp()
         .addFields(
-            { name: 'Roy Coin Charity', value: `<@${message.author.id}> just gave <@${message.mentionedUser}> a Roy Coin?!?`}
+            { name: 'Roy Coin Charity', value: `<@${message.author.id}> just gave <@${message.mentions.users.first().id}> a Roy Coin?!?`}
         )
         .setFooter('Reddit Gold Replacement?');
         message.channel.send({ embeds: [royCoinDonate] });
