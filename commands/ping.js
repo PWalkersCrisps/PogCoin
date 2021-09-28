0049e4c2e7b30f79ab53d86a02268f07fdc9a593
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-28 09:30:30 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,16 +3,18 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: "ping",
     description: "pings the server to see the delay between the client and the server",
-    async execute(Discord, client, message, args, profileData){
+    async execute(Discord, client, args, profileData){
 
-        const royCoinPing = new MessageEmbed() //Starts the proccess for creating an embed
-        .setColor('#ffff00')
-        .setTimestamp()
-        .setFooter('Reddit Gold Replacement?')
-        .addFields(
-            {name: "Ping Pong", value: `🏓Latency is ${Date.now() - message.createdTimestamp}ms.`} //Its creates a time stamp for the message then compares it to when the message is actually sent to get a mostly accurate representation of the Client/Server delay
-        );
+        client.on("messageCreate", message =>{
+            const royCoinPing = new MessageEmbed() //Starts the proccess for creating an embed
+            .setColor('#ffff00')
+            .setTimestamp()
+            .setFooter('Reddit Gold Replacement?')
+            .addFields(
+                {name: "Ping Pong", value: `🏓Latency is ${Date.now() - message.createdTimestamp}ms.`} //Its creates a time stamp for the message then compares it to when the message is actually sent to get a mostly accurate representation of the Client/Server delay
+            );
 
-        message.channel.send({ embeds: [royCoinPing] }) 
+            message.channel.send({ embeds: [royCoinPing] }) 
+        })
     }
 }
