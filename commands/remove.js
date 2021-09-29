4e20b3f786026ad3058f01c97ae08430a543c4c7
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-29 19:12:49 +0100

    Create remove.js

---
+++ commands/remove.js
@@ -0,0 +1,42 @@
+
+const mongoose = require('mongoose');
+
+module.exports = {
+    name: "remove",
+    description: "give a player some coins",
+    async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData) {
+
+        if (!args.length){
+            message.channel.send("You need to mention a member to steal them coins");
+            return;
+        }
+        const amount = args[1];
+        if (!message.mentions.users.first()) return message.channel.send("That user does not exist");
+
+        if (!(amount % 1 != 0 || amount <= 0)) return message.channel.send("Ayo if you want to actually take money make it an actual number");
+
+        try {
+        const targetData = await profileModel.findOne({ userID: message.mentions.users.first().id });
+        if (!targetData)
+        {
+            let newUser = await profileModel.create({
+                userID: message.mentions.users.first().id,
+                coins: 0,
+            });
+            //const savedUser = await newUser.save();
+        }
+
+        const response = await profileModel.findOneAndUpdate({ //finds the profile of the author then updates it
+            userID: message.mentions.users.first().id, //looks for the record of the message author's account
+        }, {
+            $inc: {
+                coins: -amount, //decreases the amount of coins that the author has by the stated amount
+            }
+        });
+
+        return message.channel.send(`<@${message.mentions.users.first().id}> has just lost ${amount} coins\n\nmake fun of :emock:`);
+        } catch (err) {
+        console.log(err);
+        }
+    },
+};
