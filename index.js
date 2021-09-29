3e0ec242ac2b382f5d85ab546392b68612634c17
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-29 23:10:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,17 +80,9 @@ client.on('messageReactionAdd', async (reaction, user) => {
     console.log(user.id);
     console.log(reaction.message.author.id)
 
-    return;
 
-    if (!message.mentions.users.first()) {
-        message.channel.send('You need to mention a user.'); //If no one was mentioned in the message then the rest of the script wont execute
-        return;
-    }
-    //let amount = 1;
-    //if (args.length) amount = args[1];
-
-    profileDataSender = await profileModel.findOne({userID: message.author.id}); //Gets the profile data of the sender
-    if(profileDataSender.coins <= 0) return message.channel.send(`<@${message.author.id}> Bruh, are you actually this broke? Try giving people coins when you actually have some roycoins <:nioCyoR:891377626831290509> <:staremock:821120707035267133>`); //Using the profile data from earlier, the bot makes a check if the user actually has any coins, if not the rest of the script wont execute, and then the bot mocks them
+    profileDataSender = await profileModel.findOne({userID: user.id}); //Gets the profile data of the sender
+    if(profileDataSender.coins <= 0) return user.send(`<@${user.id}> Bruh, are you actually this broke? Try giving people coins when you actually have some roycoins`); //Using the profile data from earlier, the bot makes a check if the user actually has any coins, if not the rest of the script wont execute, and then the bot mocks them
 
     const senderResponse = await profileModel.findOneAndUpdate({ //finds the profile of the author then updates it
         userID: message.author.id, //looks for the record of the message author's account
@@ -100,18 +92,18 @@ client.on('messageReactionAdd', async (reaction, user) => {
         }
     });
     
-    profileDataMentioned = await profileModel.findOne({userID: message.mentions.users.first().id}); //Gets the profile data of the user mentioned
+    profileDataMentioned = await profileModel.findOne({userID: reaction.message.author.id}); //Gets the profile data of the user mentioned
     if(!profileDataMentioned) //If there was no profile data of the mentioned user then it will create a new account on the database
     {
         let newUser = await profileModel.create({
-            userID: message.mentions.users.first().id,
+            userID: reaction.message.author.id,
             coins: 0,
         });
         //const savedUser = await newUser.save();
     }
 
     const reciverResponse = await profileModel.findOneAndUpdate({ //finds the profile of the user that the author mentioned then updates it
-        userID: message.mentions.users.first().id,
+        userID: reaction.message.author.id,
     }, {
         $inc: {
             coins: 1, //increases the amount of coins that the mentioned has by 1
@@ -124,7 +116,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
     .setFooter('Reddit Gold Replacement?');
 
     royCoinDonate.addFields(
-        { name: 'Roy Coin Charity', value: `<@${message.author.id}> just gave <@${message.mentions.users.first().id}> a Roy Coin?!?`}
+        { name: 'Roy Coin Charity', value: `<@${user.id}> just gave you a Roy Coin?!?`}
     )
 
 
@@ -139,7 +131,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
     //     )    
     // }
 
-    message.channel.send({ embeds: [royCoinDonate] });
+    reaction.message.author.send({ embeds: [royCoinDonate] });
 
 
 });
