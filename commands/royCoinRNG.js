31689d72aca93564643fad54322fcc887ec62afc
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-29 18:48:09 +0100

    Update royCoinRNG.js

--- commands/royCoinRNG.js
+++ commands/royCoinRNG.js
@@ -6,45 +6,47 @@ const mongoose = require("mongoose");
 module.exports = {
     name: "royCoinRNG",
     description: "When member talk without cooldown they have chance to gain coin",
-    async execute(args, message, MessageEmbed, profileModel, profileData){
+    async execute(args, MessageEmbed, profileModel, profileData){
 
-        if (!cooldowns.has(message.author.id)) { //goes to check if the cooldowns map *DOESNT* habe the author's
-            const response = await profileModel.findOneAndUpdate({
-                userID: message.author.id, //looks for the id of the author
-            }, {
-                $inc: {
-                    coins: 1, //when the id of the author is found, it gives them one coin
+        client.on("messageCreate", message => {
+            if (!cooldowns.has(message.author.id)) { //goes to check if the cooldowns map *DOESNT* habe the author's
+                const response = await profileModel.findOneAndUpdate({
+                    userID: message.author.id, //looks for the id of the author
+                }, {
+                    $inc: {
+                        coins: 1, //when the id of the author is found, it gives them one coin
+                    }
+                });
+                
+                const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
+                .setColor('#ffff00')
+                .setTimestamp()
+                .setFooter('Reddit Gold Replacement?');    
+                
+                if (message.member.roles.cache.some(role => role.name === 'he')){ //checks if the auther has the he/him role
+                    royCoinEmbedReward.addFields(
+                        { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good boy`},
+                    )        
                 }
-            });
-            
-            const royCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
-            .setColor('#ffff00')
-            .setTimestamp()
-            .setFooter('Reddit Gold Replacement?');    
-            
-            if (message.member.roles.cache.some(role => role.name === 'he')){ //checks if the auther has the he/him role
-                royCoinEmbedReward.addFields(
-                    { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good boy`},
-                )        
-            }
-            else if (message.member.roles.cache.some(role => role.name === 'her')){ //checks if the auther has the she/her role
-                royCoinEmbedReward.addFields(
-                    { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good girl`},
-                )        
-            }
-            else{ //If the user has the they/them or dont have a gender role, it will always default to this
-                royCoinEmbedReward.addFields(
-                    { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good child`},
-                )            
-            }
+                else if (message.member.roles.cache.some(role => role.name === 'her')){ //checks if the auther has the she/her role
+                    royCoinEmbedReward.addFields(
+                        { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good girl`},
+                    )        
+                }
+                else{ //If the user has the they/them or dont have a gender role, it will always default to this
+                    royCoinEmbedReward.addFields(
+                        { name: 'Roy Coin', value: `Youve been rewarded with a Roy Coin for being a good child`},
+                    )            
+                }
 
-            message.author.send({ embeds: [royCoinEmbedReward] }); //sends the embed that was just created
-            
-            cooldowns.add(message.author.id); //Adds a cooldown to the id of the author
-            setTimeout(() => {
-                // Removes the user from the set after a while
-                cooldowns.delete(message.author.id);
-            }, 1 * 60000); //First number is minutes the second one times it because it is in milliseconds        
+                message.author.send({ embeds: [royCoinEmbedReward] }); //sends the embed that was just created
+                
+                cooldowns.add(message.author.id); //Adds a cooldown to the id of the author
+                setTimeout(() => {
+                    // Removes the user from the set after a while
+                    cooldowns.delete(message.author.id);
+                }, 1 * 60000); //First number is minutes the second one times it because it is in milliseconds        
+            }
         }
     }
 }
