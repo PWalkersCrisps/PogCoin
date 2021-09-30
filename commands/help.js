eb8ff257aa502654ef1897fb884f0a65acf4436d
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-30 09:44:37 +0100

    Update help.js
    Added console.log so that i can understand some specifics

--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,8 @@ module.exports = {
         .setTimestamp()
         .setFooter('Reddit Gold Replacement?');    
 
+        console.log(cmdChoice)
+
         switch(cmdChoice){
 
             case "ping":
@@ -19,35 +21,35 @@ module.exports = {
                     {name: "Permissions", value: "Anyone can use it"},
                     {name: "Usage", value: "**>ping**"},
                     {name: "Command", value: "Pings the server this bot is hosted on so that you can see the delay between discord and it"}
-                )
+                );
                 break;
             case "balance":
                 royCoinEmbedHelp.addfields(
                     {name: "Permissions", value: "Anyone can use it"},
                     {name: "Usage", value: "**>balance**"},
                     {name: "Command", value: "Checks how much roycoins you have"}
-                )
+                );
                 break;
             case "donate":
                 royCoinEmbedHelp.addfields(
                     {name: "Permissions", value: "Anyone can use it"},
                     {name: "Usage", value: "**>donate**"},
                     {name: "Command", value: "Donates 1 roycoin to the mentioned user, alternativly you react to someones message with :roycoin:"}
-                )
+                );
                 break;
             case "give":
                 royCoinEmbedHelp.addfields(
                     {name: "Permissions", value: "Only admins can use it"},
                     {name: "Usage", value: "**>give [amount]"},
                     {name: "Command", value: "Give [amount]\nWith this you can make fun of people who gained more money"}
-                )
+                );
                 break;
             case "remove":
                 royCoinEmbedHelp.addfields(
                     {name: "Permissions", value: "Only admins can use it"},
                     {name: "Usage", value: "**>remove [amount]"},
                     {name: "Command", value: "Removes [amount]\nWith this you can make fun of people who lost their money"}
-                )
+                );
                 break;
         }
 
