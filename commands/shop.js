e8d61241aebe0cd67a0fc583d61f0da9350b7f3c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-30 19:09:37 +0100

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -7,16 +7,18 @@ module.exports = {
 
         if (items.length === 0) return message.channel.send(`<@${message.author.id}> Unfortunatly im not selling right now, ig you just need to be patient`);
 
-        const shoplist = items.map((value, index) =>{
+        const itemlist = items.map((value, index) =>{
             return `\n**${index + 1})** ${value.item} -> ${value.price} coins!`
         });
+
+        let shoplist = itemlist.replaceAll(",", "");
 
         const shoplistEmbed = new MessageEmbed()
         .setColor("#7de48b")
         .setTimestamp()
         .setFooter("Steam Shop Replacement?")
         .addFields(
-            {name: "Roy Shop", value: `${shoplist.replaceAll(",", "")}`}
+            {name: "Roy Shop", value: `${shoplist}`}
         );
 
         message.channel.send({ embeds: [shoplistEmbed] })
