b317f937f78606c807811f4052507e39357e1a71
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-09-30 13:56:13 +0100

    Added shop

---
+++ economy/shop.js
@@ -0,0 +1,18 @@
+const items = require("./shopitems")
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "shop",
+    async execute(Discord, client, args, message, profileModel, profileData){
+
+        if (items.length === 0) return message.channel.send(`<@${message.author.id}> Unfortunatly im not selling right now, ig you just need to be patient`);
+
+        const shoplist = items
+        .map((value, index) =>{
+            return `**${index+1}** ${value.item} -> ${value.price}coins`
+        })
+
+        message.channel.send(shoplist);
+
+    }
+}
---
+++ economy/shopitems.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        item: "PWC's magic sword",
+        price: "20"
+    },
+    {
+        item: "Roy's staff of forgiving",
+        price: "30"
+    },
+    {
+        item: "Spook's ghostly book",
+        price: "25"
+    },
+]
