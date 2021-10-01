a8630baf9981e8769a06d3adefcea35172b41778
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-01 07:22:32 +0100

    Changed small things
    changed role names in shoplist to not have spaces
    decreasted the chance of getting a roy coin from 1/10 to 1/100

--- economy/shopitems.js
+++ economy/shopitems.js
@@ -1,16 +1,16 @@
 module.exports = [
     {
-        item: "Role 1",
+        item: "Role1",
         price: "20",
         roleid: "893237084586917918",
     },
     {
-        item: "Role 2",
+        item: "Role2",
         price: "60",
         roleid: "893237109853401139",
     },
     {
-        item: "Role 3",
+        item: "Role3",
         price: "120",
         roleid: "893237304620118056",
     },
