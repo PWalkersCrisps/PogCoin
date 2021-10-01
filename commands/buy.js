23a0e92f6be4bbbab7ce17b90c62bd9c0e5cf19b
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-01 16:36:02 +0100

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,10 +1,10 @@
 const items = require("../economy/shopitems");
-
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "buy",
     description: "pings the server to see the delay between the client and the server",
-    async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData){
+    async execute(Discord, client, args, message, profileModel, profileData){
 
         if(!args[0]) return message.channel.send("Actually try to buy something?")
         const itemToBuy = args[0].toLowerCase();
@@ -15,9 +15,28 @@ module.exports = {
         const itemPrice = items.find((val) => (val.item.toLowerCase()) === itemToBuy).price;
         if(profileData.coins < itemPrice) return message.channel.send("Man... youre broke, get more more roycoins");
 
-        console.log();
+        const response = await profileModel.findOneAndUpdate({ //finds the profile of the author then updates it
+            userID: message.author.id, //looks for the record of the message author's account
+        }, {
+            $inc: {
+                coins: -itemPrice, //decreases the amount of coins that the author has by the stated amount
+            }
+        });
 
-        message.member.roles.add(message.guild.roles.cache.find(r => r.id === "893237084586917918"))
+        const roleGive = items.find((val) => (val.item.toLowerCase()) === itemToBuy).roleid;
+
+        message.member.roles.add(message.guild.roles.cache.find(r => r.id === roleGiveID));
+
+        const royCoinDonate = new MessageEmbed()
+        .setColor('#00ffff')
+        .setTimestamp()
+        .setFooter('Amazon Replacement?')
+        .addFields(
+            {name: "royshop", value: `wowza, moneybags <@${message.author.id}> just got <@&${message.guild.roles.cache.find(r => r.id === roleGiveID)}>`}
+        )
+
+        message.channel.send({ embeds: [royCoinDonate] })
+
 
     }
 }
