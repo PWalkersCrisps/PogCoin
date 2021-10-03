76c5749f3d70758889ed9a24143726fe813f84c8
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-03 17:38:01 +0100

    Update balance.js
    -changed cooldown
    -Made it so that you can check others balance

--- commands/balance.js
+++ commands/balance.js
@@ -5,16 +5,29 @@ module.exports = {
     description: "check your balance",
     async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData){
         if (cooldowns.has(message.author.id)) { //checks if the author currently has a cooldown on this command
-            message.channel.send(`Dont think Roy is gonna be too happy with you spamming\n\nPlease can you wait like 1 minute?`); 
+            message.channel.send(`Dont think Roy is gonna be too happy with you spamming\n\nPlease can you wait like 30 or so seconds?`); 
         } else { //if the author doesnt have a cooldown then this code executes
+
+            const userPinged = message.mentions.users.first();
 
             const royCoinBalance = new MessageEmbed()
             .setColor('#ff00ff')
             .setTimestamp()
-            .addFields(
-                { name: 'Roy Coin Bank', value: `You have ${profileData.coins}<:RoyCoin:891377698922958879>`}
-            )
             .setFooter('Reddit Gold Replacement?');
+
+            if(userPinged === undefined){
+                royCoinBalance.addFields(
+                    { name: 'Roy Coin Bank', value: `You have ${profileData.coins} roycoins`}
+                )    
+            }
+            else{
+                const profileDataPinged = profileData = await profileModel.findOne({userID: userPinged.id}); //Attempts to look for a user in the DB with the user's id
+
+                royCoinBalance.addFields(
+                    { name: 'Roy Coin Bank', value: `You have ${profileDataPinged.coins}<:RoyCoin:891377698922958879>`}
+                )    
+            }
+
             message.channel.send({ embeds: [royCoinBalance] });
 
 
@@ -22,7 +35,7 @@ module.exports = {
             setTimeout(() => {
                // Removes the user from the set after a while
                 cooldowns.delete(message.author.id);
-            }, 60 * 1000);
+            }, 30 * 1000);
         }
     }
 }
