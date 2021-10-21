9e05ff28d095e91d99880c45e72bf9cbcb86da13
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-21 16:06:26 +0100

    lol

--- arrays/shopitems.js
+++ arrays/shopitems.js
@@ -4,35 +4,5 @@ module.exports = [
         price: "15",
         roleid: "893237084586917918",
     },
-    {
-        item: "Role2",
-        price: "15",
-        roleid: "893237084586917918",
-    },
-    {
-        item: "Role3",
-        price: "15",
-        roleid: "893237084586917918",
-    },
-    {
-        item: "Role4",
-        price: "15",
-        roleid: "893237084586917918",
-    },
-    {
-        item: "Role5",
-        price: "15",
-        roleid: "893237084586917918",
-    },
-    {
-        item: "Role6",
-        price: "15",
-        roleid: "893237084586917918",
-    },
-    {
-        item: "Role7",
-        price: "15",
-        roleid: "893237084586917918",
-    },
 
 ]
