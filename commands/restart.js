e40bed90f631a8cbe890974bfb0f0a91a8f5b52c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-21 20:46:19 +0100

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: "restart",
+    description: "restarts the bot",
+    async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData){
+
+        const restartEmbed = new MessageEmbed() //Starts the proccess for creating an embed
+        .setColor('#afa00a')
+        .setTimestamp()
+        .setFooter('Some kind of replacement for something?')
+        .addFields(
+            {name: "Are you sure you want to restart", value: "React with ✅ to continue restarting \nReact with ❌ to stop restarting\n\n**WARNING: THIS WILL RESTART THE CURRENT LIST OF COOLDOWNS**"}
+        );
+
+        message.channel.send({ embeds: [restartEmbed] }) 
+        .then(msg=> { msg.react("✅") })
+        .then(msg=> { msg.react("❌") })
+        .catch();
+
+        const filter = (reaction, user) => {
+            return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.user.id;
+        };
+
+        if (reaction.emoji.name === '✅') {
+            message.channel.send('Resetting...')
+            .then(message => client.destroy())
+            .then(() => client.login(process.env.DISCORD_TOKEN));
+        } 
+        else if (reaction.emoji.name === '❌') {
+            message.channel.send("Whatever suits you...")
+        }
+    }
+}
