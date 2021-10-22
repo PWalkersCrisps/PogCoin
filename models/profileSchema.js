bcf10c44655bb544e0f775ab67b94e926647a35c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-22 19:35:12 +0100

    Update profileSchema.js

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 //How the fuck do you expect me to explain this??
 const profileSchema = new mongoose.Schema({
     userID: {type: mongoose.SchemaTypes.String, require: true, unique: true},
-    coins: {type: mongoose.SchemaTypes.Number, default: 0},
+    coins: {type: mongoose.SchemaTypes.Number, default: 1},
+    robTimestamp: {type: mongoose.SchemaTypes.Timestamp},
+    totalCoinsEarnt: {type: mongoose.SchemaTypes.Number},
+    coinsDonated: {type: mongoose.SchemaTypes.Number},
+    coinsReceived: {type: mongoose.SchemaTypes.Number},
+    netGamble: {type: mongoose.SchemaTypes.Number},
+    robSuccess: {type: mongoose.SchemaTypes.Number},
+    robFails: {type: mongoose.SchemaTypes.Number},
+    timesRobbed: {type: mongoose.SchemaTypes.Number},
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
