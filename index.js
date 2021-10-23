c77961f62a0e72e6bc26418621e8c8ffee1499a3
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-23 22:14:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
-    require(`.handlers/${handler}`)(client, Discord)
+    const handle = require(`.handlers/${handler}`)(client, Discord)
 })
 
 ///-----Mongoose-----///
