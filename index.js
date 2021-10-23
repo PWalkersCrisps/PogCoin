2ae97e851ae3ebf25873c4c1881200dfe7eaa058
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-24 00:56:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ require("dotenv").config();
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler => {
-    const handle = require(`.handlers/${handler}`)(client, Discord)
-})
+try{
+    ['command_handler', 'event_handler'].forEach(handler => {
+        require(`.handlers/${handler}`)(client, Discord)
+    })
+}
+catch(err){
+    console.log(err);
+}
 
 ///-----Mongoose-----///
 mongoose.connect(process.env.MONGODB_SRV, { //idk what this shit does
