f892ce0ebd7d995d3b5def05ae9ea72dac88f60c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-24 13:31:25 +0100

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -91,37 +91,6 @@ module.exports = async(Discord, client, message) =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    //-----Cooldowns-----///
-    try{
-        const collection = new Discord.Collection();
-
-        if(!cooldowns.has(command.name)){
-            cooldowns.set(command.name, collection());
-        }
-    
-        const current_time = Date.now();
-        const time_stamps = cooldowns.get(command.name);
-        const cooldown_amount = (command.cooldown) * 1000;
-    
-        //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
-        if(time_stamps.has(message.author.id)){
-            const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-    
-            if(current_time < expiration_time){
-                const time_left = (expiration_time - current_time) / 1000;
-    
-                return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
-            }
-        }
-    
-        //If the author's id is not in time_stamps then add them with the current time.
-        time_stamps.set(message.author.id, current_time);
-        //Delete the user's id once the cooldown is over.
-        setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
-    }
-    catch(err){
-        console.log(err);
-    }
     ///-----CMD execution-----///
 
     try{
