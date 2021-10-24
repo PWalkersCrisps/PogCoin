d5935357713168e32a8c25bb70bb886583e99ed1
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-24 10:00:18 +0100

    Update event_handler.js

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 module.exports = (client, Discord) =>{
-    const load_dir = (dir) =>{
-        const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith(".js"));
+    const load_dir = (dirs) =>{
+        const eventFiles = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith(".js"));
 
-        for(const file of event_files){
+        for(const file of eventFiles){
             const event = require(`../events/${dirs}/${file}`);
             const event_name = file.split('.')[0];
             client.on(event_name, event.bind(null, client, Discord))
