fc82255418ad5ddf60ab1b9dec5500f4243afc8e
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-24 13:50:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 try{
-    ['command_handler', 'event_handler'].forEach(handler => {
+    ['event_handler'].forEach(handler => {
         require(`./handlers/${handler}`)(client, Discord)
     })
 }
