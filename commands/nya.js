c6d5de44c198b93f9999472eb6208f87bfd7487e
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-27 19:13:51 +0100

    Update nya.js

--- commands/nya.js
+++ commands/nya.js
@@ -1,22 +1,33 @@
 const lewdImages = require("../arrays/lewdImages")
+const talkedRecently = new Set();
 
 module.exports = {
     name: "nya",
     description: "uwu",
     async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData){
         try{
-            const random_hex_color_code = () => {
-                let n = (Math.random() * 0xfffff * 1000000).toString(16);
-                return '#' + n.slice(0, 6);
-            };
+            if (talkedRecently.has(msg.author.id)) {
+                msg.channel.send("Wait 10 minutes before getting typing this again. - " + msg.author);
+            } else {
+                const random_hex_color_code = () => {
+                    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+                    return '#' + n.slice(0, 6);
+                };
 
-            let uwuEmbed = new MessageEmbed()
-            .setImage(lewdImages[Math.floor(Math.random() * lewdImages.length)])
-            .setColor(random_hex_color_code())
-            .setTitle("Damn these people really being horny???")
-            .setFooter("If you see anyone else being horny on main, screenshot it and send it to PWC");
+                let uwuEmbed = new MessageEmbed()
+                .setImage(lewdImages[Math.floor(Math.random() * lewdImages.length)])
+                .setColor(random_hex_color_code())
+                .setTitle("Damn these people really being horny???")
+                .setFooter("If you see anyone else being horny on main, screenshot it and send it to PWC");
 
-            message.channel.send({ embeds: [uwuEmbed] });
+                message.channel.send({ embeds: [uwuEmbed] });
+                // Adds the user to the set so that they can't talk for a minute
+                talkedRecently.add(msg.author.id);
+                setTimeout(() => {
+                // Removes the user from the set after a minute
+                talkedRecently.delete(msg.author.id);
+                }, 600 * 1000);
+            }
         }
         catch(err){
             console.log(err);
