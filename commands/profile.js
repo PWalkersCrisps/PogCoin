c1823330d7594e2bd0a659aff9eb7695c1e167a9
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-28 00:50:35 +0100

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -19,7 +19,7 @@ module.exports = {
     
         }
         else{
-            let profileDataPinged = profileData = await profileModel.findOne({userID: userPinged.id}); //Attempts to look for a user in the DB with the user's id
+            let profileDataPinged = await profileModel.findOne({userID: userPinged.id}); //Attempts to look for a user in the DB with the user's id
             if(!profileDataPinged) //If there was no profile data of the mentioned user then it will create a new account on the database
             {
                 let newUser = await profileModel.create({
