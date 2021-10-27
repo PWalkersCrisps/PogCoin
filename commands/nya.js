9dc441a3ac1207f07af8cb4f14c4a2468a944fc7
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-27 18:09:30 +0100

    Update nya.js

--- commands/nya.js
+++ commands/nya.js
@@ -5,8 +5,17 @@ module.exports = {
     description: "uwu",
     async execute(Discord, client, args, message, MessageEmbed, profileModel, profileData){
         try{
+            const random_hex_color_code = () => {
+                let n = (Math.random() * 0xfffff * 1000000).toString(16);
+                return '#' + n.slice(0, 6);
+            };
+
             let uwuEmbed = new MessageEmbed()
-            .setImage(lewdImages[Math.floor(Math.random() * lewdImages.length)]);
+            .setImage(lewdImages[Math.floor(Math.random() * lewdImages.length)])
+            .setColor(random_hex_color_code())
+            .setTitle("Damn these people really being horny???")
+            .setFooter("If you see anyone else being horny on main, screenshot it and send it to PWC");
+
             message.channel.send({ embeds: [uwuEmbed] });
         }
         catch(err){
