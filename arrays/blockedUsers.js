7eed32afa56faf370a420241a8abe7aa3df654aa
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-10-28 22:39:44 +0100

    Update blockedUsers.js

--- arrays/blockedUsers.js
+++ arrays/blockedUsers.js
@@ -1,10 +1,5 @@
 module.exports = [
-    {
-        id: "718802920186970202",
-        reason: "Just being in the wrong gene pool",
-    },
-    {
-        id: "485528066647523339",
-        reason: "fuck you catro\n thanks for testing",
-    }
+    "718802920186970202",
+    "485528066647523339",
+
 ]
