530e7179cd08ae53631a285a1c3a3cecefa7c891
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-11-12 08:09:06 +0000

    update coinRNG

--- index.js
+++ index.js
@@ -44,6 +44,51 @@ mongoose.connect(process.env.MONGODB_SRV, { //idk what this shit does
 ///-----Executes When Message Is Created-----///
 client.on("messageCreate", async(message) => {
 
+    ///-----Pogcoin RNG-----//
+
+    if (Math.random() < 0.1){
+
+        if (!coinCooldown.has(message.author.id)) { //goes to check if the cooldowns map *DOESNT* habe the author's
+            const response = await profileModel.findOneAndUpdate({
+                userID: message.author.id, //looks for the id of the author
+            }, {
+                $inc: {
+                    coins: 1, //when the id of the author is found, it gives them one coin
+                    totalCoinsEarnt: 1
+                }
+            });
+            
+
+            const pogCoinEmbedReward = new MessageEmbed() //Starts the proccess for creating an embed
+            .setColor('#ffff00')
+            .setTimestamp()
+            .setFooter('Reddit Gold Replacement?');    
+            
+            if (message.member.roles.cache.some(role => role.name === 'he/him')){ //checks if the auther has the he/him role
+                pogCoinEmbedReward.addFields(
+                    { name: 'pog Coin', value: `Youve been rewarded with a pog Coin for being a good boy`},
+                )        
+            }
+            else if (message.member.roles.cache.some(role => role.name === 'she/her')){ //checks if the auther has the she/her role
+                pogCoinEmbedReward.addFields(
+                    { name: 'pog Coin', value: `Youve been rewarded with a pog Coin for being a good girl`},
+                )        
+            }
+            else{ //If the user has the they/them or dont have a gender role, it will always default to this
+                pogCoinEmbedReward.addFields(
+                    { name: 'pog Coin', value: `Youve been rewarded with a pog Coin for being a good child`},
+                )            
+            }
+
+            message.author.send({ embeds: [pogCoinEmbedReward] }); //sends the embed that was just created
+            
+            coinCooldown.add(message.author.id); //Adds a cooldown to the id of the author
+            setTimeout(() => {
+                // Removes the user from the set after a while
+                coinCooldown.delete(message.author.id);
+            }, 60 * 60000); //First number is minutes the second one times it because it is in milliseconds        
+        }
+    }
 
     let profileData;
     try{
