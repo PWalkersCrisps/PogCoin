a8778afa45d06d38d84bc683f09f2d44e8f81efa
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-12 21:49:41 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,29 +5,24 @@ module.exports = {
     description: 'pings the server to see the delay between the client and the server',
     data: new SlashCommandBuilder().setName('ping')
     .setDescription('Replies with server latinency'),
-    async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileModel, profileData) {
+    async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileSchema, cooldownSchema, profileData) {
 
-        try {
-            const pogCoinPing = new MessageEmbed() // Starts the proccess for creating an embed
-            .setColor('#ffff00')
-            .setTimestamp()
-            .setFooter('Reddit Gold Replacement?')
-            .addFields(
-                { name: 'Ping Pong', value: `🏓Latency is ${Date.now() - interaction.createdTimestamp}ms.` }, // Its creates a time stamp for the message then compares it to when the message is actually sent to get a mostly accurate representation of the Client/Server delay
-            );
+        const pogCoinPing = new MessageEmbed() // Starts the proccess for creating an embed
+        .setColor('#ffff00')
+        .setTimestamp()
+        .setFooter('Reddit Gold Replacement?')
+        .addFields(
+            { name: 'Ping Pong', value: `🏓Latency is ${Date.now() - interaction.createdTimestamp}ms.` }, // Its creates a time stamp for the message then compares it to when the message is actually sent to get a mostly accurate representation of the Client/Server delay
+        );
 
-            const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('primary')
+                .setLabel('Primary')
+                .setStyle('PRIMARY'),
+        );
 
-            interaction.reply({ embeds: [pogCoinPing], components: [row] });
-        }
-        catch (err) {
-            console.error(err);
-        }
+        interaction.reply({ embeds: [pogCoinPing], components: [row] });
     },
 };
