5c39a126f58141652876e8a3c6ad05afbf72bf75
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-12 21:49:49 +0000

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -5,27 +5,22 @@ module.exports = {
     name: 'shop',
     data: new SlashCommandBuilder().setName('shop')
     .setDescription('view what you want to buy'),
-    async execute(client, interaction, MessageEmbed, profileModel, profileData) {
-        try {
-            if (items.length === 0) return interaction.reply({ content: `<@${interaction.user.id}> i'm not selling right now, ig you just need to be patient`, ephemeral: true });
+    async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileSchema, cooldownSchema, profileData) {
+        if (items.length === 0) return interaction.reply({ content: `<@${interaction.user.id}> i'm not selling right now, ig you just need to be patient`, ephemeral: true });
 
-            const shoplistEmbed = new MessageEmbed()
-            .setColor('#7de48b')
-            .setTimestamp()
-            .setFooter('Steam Shop Replacement?')
-            .setTitle('pog Shop')
-            .setDescription(`Use ${process.env.DISCORD_PREFIX}buy to buy a role`);
+        const shoplistEmbed = new MessageEmbed()
+        .setColor('#7de48b')
+        .setTimestamp()
+        .setFooter('Steam Shop Replacement?')
+        .setTitle('pog Shop')
+        .setDescription(`Use ${process.env.DISCORD_PREFIX}buy to buy a role`);
 
-            const itemlist = items.map((value, index) => {
-                return shoplistEmbed.addFields(
-                    { name: `**${index + 1})** ${value.item}`, value: `${value.price} pogcoins!` },
-                );
-            });
+        const itemlist = items.map((value, index) => {
+            return shoplistEmbed.addFields(
+                { name: `**${index + 1})** ${value.item}`, value: `${value.price} pogcoins!` },
+            );
+        });
 
-            interaction.reply({ embeds: [shoplistEmbed], ephemeral: true });
-        }
-        catch (err) {
-            console.error(err);
-        }
+        interaction.reply({ embeds: [shoplistEmbed], ephemeral: true });
     },
 };
