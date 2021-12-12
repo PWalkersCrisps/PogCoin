f0e25aba1dc6b1bf2cf8f39da3d24247ba031ec7
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-12 21:50:15 +0000

    Update gamble.js

--- commands/gamble.js
+++ commands/gamble.js
@@ -6,58 +6,53 @@ module.exports = {
     data: new SlashCommandBuilder().setName('gamble')
     .setDescription('Gamble all of your life savings away')
     .addIntegerOption(option => option.setName('amount').setDescription('How much do you want to gamble?')),
-    async execute(client, interaction, MessageEmbed, profileModel, profileData) {
-        try {
-            const amount = interaction.options.getInteger('int');
-            if (amount < 1) return interaction.reply({ content: `<${interaction.user.id}> Please can you actually try to gamble something?`, ephemeral: true });
-            if (profileData.coins < amount) return interaction.reply({ content: 'Actually have enough coins??', ephemeral: true });
+    async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileSchema, cooldownSchema, profileData) {
+        const amount = interaction.options.getInteger('int');
+        if (amount < 1) return interaction.reply({ content: `<${interaction.user.id}> Please can you actually try to gamble something?`, ephemeral: true });
+        if (profileData.coins < amount) return interaction.reply({ content: 'Actually have enough coins??', ephemeral: true });
 
 
-            const pogCoinGamble = new MessageEmbed() // Starts the proccess for creating an embed
-            .setTimestamp();
+        const pogCoinGamble = new MessageEmbed() // Starts the proccess for creating an embed
+        .setTimestamp();
 
-            let probability;
-            if (amount > 70) probability = 0.2;
-            else probability = ((-amount + 100) / 2) / 75;
+        let probability;
+        if (amount > 70) probability = 0.2;
+        else probability = ((-amount + 100) / 2) / 75;
 
-            if (Math.random() < probability) {
-                const response = await profileModel.findOneAndUpdate({
-                    userID: interaction.user.id, // looks for the id of the author
-                }, {
-                    $inc: {
-                        coins: amount, // when the id of the author is found, it gives them one coin
-                        totalCoinsEarnt: amount * 2,
-                        netGamble: amount,
-                    },
-                });
-                pogCoinGamble
-                .setFooter('Holy shit you actually won?')
-                .setColor('YELLOW')
-                .addFields(
-                    { name: '!!!YOU WON!!!', value: `<@${interaction.user.id}> HOLY FUCKING SHIT YOU ACTUALLY WON? I PROBABLY SHOULD GIVE YOU ${amount * 2} <:pogcoin:899662337399750666>` },
-                );
-            }
-            else {
-                const response = await profileModel.findOneAndUpdate({
-                    userID: interaction.user.id, // looks for the id of the author
-                }, {
-                    $inc: {
-                        coins: -amount, // when the id of the author is found, it gives them one coin
-                        netGamble: -amount,
-                    },
-                });
-                pogCoinGamble
-                .setFooter('Vegas Replacement?')
-                .setColor('RED')
-                .addFields(
-                    { name: 'YOU LOST!', value: `<@${interaction.user.id}> omg you actually lost ${amount} <:pogcoin:899662337399750666>, i really want to make fun of you` },
-                );
-            }
-
-            interaction.reply({ embeds: [pogCoinGamble] });
+        if (Math.random() < probability) {
+            const response = await profileSchema.findOneAndUpdate({
+                userID: interaction.user.id, // looks for the id of the author
+            }, {
+                $inc: {
+                    coins: amount, // when the id of the author is found, it gives them one coin
+                    totalCoinsEarnt: amount * 2,
+                    netGamble: amount,
+                },
+            });
+            pogCoinGamble
+            .setFooter('Holy shit you actually won?')
+            .setColor('YELLOW')
+            .addFields(
+                { name: '!!!YOU WON!!!', value: `<@${interaction.user.id}> HOLY FUCKING SHIT YOU ACTUALLY WON? I PROBABLY SHOULD GIVE YOU ${amount * 2} <:pogcoin:899662337399750666>` },
+            );
         }
-        catch (err) {
-            console.error(err);
+        else {
+            const response = await profileSchema.findOneAndUpdate({
+                userID: interaction.user.id, // looks for the id of the author
+            }, {
+                $inc: {
+                    coins: -amount, // when the id of the author is found, it gives them one coin
+                    netGamble: -amount,
+                },
+            });
+            pogCoinGamble
+            .setFooter('Vegas Replacement?')
+            .setColor('RED')
+            .addFields(
+                { name: 'YOU LOST!', value: `<@${interaction.user.id}> omg you actually lost ${amount} <:pogcoin:899662337399750666>, i really want to make fun of you` },
+            );
         }
+
+        interaction.reply({ embeds: [pogCoinGamble] });
     },
 };
