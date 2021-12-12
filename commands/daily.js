102d966c020880d9803043c5fde8ea583c8ccd5c
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2021-12-12 10:16:50 +0000

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -6,63 +6,57 @@ module.exports = {
     data: new SlashCommandBuilder().setName('daily')
     .setDescription('Get your daily coin, but beware, there is a 50% chance of getting nothing!'),
 
-    async execute(client, interaction, MessageEmbed, profileModel, profileData) {
-        try {
-            if (profileData.dailyTimestamp + 86400 <= Date.now() / 1000) {
+    async execute(client, interaction, MessageEmbed, MessageActionRow, MessageButton, profileSchema, cooldownSchema, profileData) {
 
-                const pogCoinDaily = new MessageEmbed()
-                .setColor('#ab5612')
-                .setTimestamp()
-                .setFooter('Wages in america replacement?');
+        if (parseFloat(profileData.dailyTimestamp) + 86400 <= Date.now() / 1000) {
 
-                if (Math.random() < 0.5) {
-                    await profileModel.findOneAndUpdate({
-                        userID: interaction.user.id,
-                    }, {
-                        $set: {
-                            dailyTimestamp: Date.now() / 1000,
-                        },
-                        $inc: {
-                            coins: 1,
-                        },
+            const pogCoinDaily = new MessageEmbed()
+            .setColor('#ab5612')
+            .setTimestamp()
+            .setFooter('Wages in america replacement?');
 
-                    });
-                    pogCoinDaily.addFields(
-                        {
-                            name: 'Daily coins',
-                            value: `<@${interaction.user.id}> you gained a coin have a nice time`,
-                        },
-                    );
+            if (Math.random() < 0.5) {
+                await profileSchema.findOneAndUpdate({
+                    userID: interaction.user.id,
+                }, {
+                    $inc: {
+                        coins: 1,
+                    },
 
+                });
+                pogCoinDaily.addFields(
+                    {
+                        name: 'Daily coins',
+                        value: `<@${interaction.user.id}> you gained a coin have a nice time`,
+                    },
+                );
 
-                }
-                else {
-                    await profileModel.findOneAndUpdate({
-                        userID: interaction.user.id,
-                    }, {
-                        $set: {
-                            dailyTimestamp: Date.now() / 1000,
-                        },
-
-                    });
-                    pogCoinDaily.addFields(
-                        {
-                            name: 'Daily coins',
-                            value: `<@${interaction.user.id}> nah, lol. no coin for you`,
-                        },
-                    );
-                }
-
-
-                interaction.reply({ embeds: [pogCoinDaily] });
 
             }
             else {
-                interaction.reply({ content: `<@${interaction.user.id}> you are still on cooldown, you just need to wait ${profileData.dailyTimestamp + 86400 - Date.now() / 1000} seconds\n\n\n do the math yourself, NERD!!` });
+                pogCoinDaily.addFields(
+                    {
+                        name: 'Daily coins',
+                        value: `<@${interaction.user.id}> nah, lol. no coin for you`,
+                    },
+                );
             }
+
+            await cooldownSchema.findOneAndUpdate({
+                userID: interaction.user.id,
+            }, {
+                $set: {
+                    dailyTimestamp: Date.now() / 1000,
+                },
+
+            });
+
+
+            interaction.reply({ embeds: [pogCoinDaily] });
+
         }
-        catch (err) {
-            console.error(err);
+        else {
+            interaction.reply({ content: `<@${interaction.user.id}> you are still on cooldown, you just need to wait ${parseFloat(profileData.dailyTimestamp) + 86400 - Date.now() / 1000} seconds\n\n\n do the math yourself, NERD!!` });
         }
     },
 };
